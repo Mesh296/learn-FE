4b4e6358433974c1893fb2954e02c76851af6c2a
Minh Tran <trumtalon296@MacBook-Air-cua-Minh.local>
2023-07-25 13:40:19 +0700

    feat: create auto complete map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import PlacesAutocomplete,
+{
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng
+}
+  from 'react-places-autocomplete';
+
+
 
 function App() {
+
+  const [address, setAddress] = useState("")
+
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null
+  });
+
+  const handleSelect = async value => {
+    const result = await geocodeByAddress(value);
+    const latlong = await getLatLng(result[0])
+    setAddress(value);
+    setCoordinates(latlong);
+    console.log(value.description.regi);
+
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) =>
+        (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+
+            <div>
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                //-------------------------------
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                  onClick={() => handleSelect(suggestion.description)}
+                  >
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+            <div>--------------------------</div>
+            <div>Latitude: {coordinates["lat"]}</div>
+            <div>Longitude: {coordinates["lng"]}</div>
+          </div>
+        )}
+      </PlacesAutocomplete>
     </div>
   );
 }
