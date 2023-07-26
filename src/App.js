0386b80dd79922929428bae4a588e356ead2cbc6
Minh Tran <trumtalon296@192.168.1.59>
2023-07-26 10:15:20 +0700

    get shipping fee of each country

--- src/App.js
+++ src/App.js
@@ -12,30 +12,50 @@ import PlacesAutocomplete,
 
 function App() {
 
-  const [address, setAddress] = useState("")
-  const [postal_code, setPostal_code] = useState("")
-  const [searchedadd, setSearchedadd] = useState("")
-  const [coordinates, setCoordinates] = useState({
+  const [address, setAddress] = useState("") //address tren search bar
+  const [postal_code, setPostal_code] = useState("") //postal code 
+  const [searchedAdd, setSearchedAdd] = useState("") //selected address
+  const [coordinates, setCoordinates] = useState({ //toa do
     lat: null,
     lng: null
   });
-  const [country, setCountry] = useState("")
+  const [country, setCountry] = useState("") //ten country
+  const [shippingFee, setShippingFee] = useState("") //phi ship 
 
+  //lay du lieu country voi shipping fee
+  const getCountryData = async (nameCountry) => {
+    await fetch('./CountryCodes.json')
+    .then(res => res.json())
+    .then(data => getFee(nameCountry, data))
+  }
+
+
+  //lay shipping fee cua country da duoc chon
+  const getFee = async (countryName, data) => {
+    const countryData = await (data.find(nameC => nameC.name === countryName))
+    setShippingFee(countryData.dial_code.slice(1)) //update shipping fee len man hinh
+  }
+
+
+  //show thong tin dia chi len man hinh
   const handleSelect = async (value, placeId) => {
-    const result = await geocodeByAddress(value);
-    const latlong = await getLatLng(result[0])
-    setAddress(value);
-    setSearchedadd(value);
-    setCoordinates(latlong);
+    const result = await geocodeByAddress(value); //lay du lieu country
+    const latlong = await getLatLng(result[0]) //lay toa do country
+    setAddress(value); //update dia chi tren search bar
+    setSearchedAdd(value); //update dia chi len man hinh
+    setCoordinates(latlong); //update toa do len man hinh
     
-    const [place] = await geocodeByPlaceId(placeId);
+    const [place] = await geocodeByPlaceId(placeId); //get ID country
     const { long_name: postalCode = '' } =
-    place.address_components.find(c => c.types.includes('postal_code')) || {};
-    setPostal_code(postalCode);
+    place.address_components.find(c => c.types.includes('postal_code')) || {}; //get Postal code
+    setPostal_code(postalCode); //update Postal code len man hinh
 
-    const { long_name: contr = ''} = result[0].address_components.find(c => c.types.includes('country')) || {};
-    setCountry(contr);
-    console.log(result[0].address_components);
+    const { long_name: countr = ''} = 
+    result[0].address_components.find(c => c.types.includes('country')) || {}; //get country name
+
+    setCountry(countr); //update country name len man hinh
+    
+    getCountryData(countr); //fetch api
 
    }
 
@@ -74,7 +94,6 @@ function App() {
                   })}
                   onClick={() => handleSelect(suggestion.description, suggestion.placeId)}
                   >
-                    {/* {console.log(suggestion.placeId)} */}
                     {suggestion.description}
                   </div>
                 );
@@ -83,9 +102,10 @@ function App() {
             <div>--------------------------</div>
             <div>Latitude: {coordinates["lat"]}</div>
             <div>Longitude: {coordinates["lng"]}</div>
-            <div>Address: {searchedadd}</div>
+            <div>Address: {searchedAdd}</div>
             <div>Postal Code: {postal_code}</div>
             <div>Country: {country}</div>
+            <div>Shipping fee: {shippingFee}</div>
           </div>
         )}
       </PlacesAutocomplete>
